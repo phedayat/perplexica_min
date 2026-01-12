a629b6ac094d4a84355f88c9c51fee4e0db34804
phedayat <phedayat@ucsd.edu>
2026-01-11 20:29:25 -0600

    Added checkbox for controlling showing reasoning

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -57,6 +57,8 @@ const MessageBox = ({
     messages,
     researchEnded,
     chatHistory,
+    showReasoning,
+    setShowReasoning
   } = useChat();
 
   const parsedMessage = section.parsedTextBlocks.join('\n\n');
@@ -280,6 +282,10 @@ const MessageBox = ({
               query={section.message.query}
               messageId={section.message.messageId}
             />
+            <label>
+              <input type="checkbox" checked={showReasoning} onChange={(e) => setShowReasoning(e.target.checked)}/>
+              Show Reasoning
+            </label>
           </div>
         )}
       </div>
